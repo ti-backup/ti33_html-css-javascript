a4efdd0c1c6af4dece3b33699a4c2b8a4d7f1a7e
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-12-11 17:30:44 -0300

    inicio do desenvolvimento de um slideshow

--- jquery/js/efeitos.js
+++ jquery/js/efeitos.js
@@ -1,5 +1,33 @@
 $(document).ready(function(){
+    $("h1").click(function(){
+        $(this).toggleClass("destaque");
+    });
+
     $("#hide").click(function(){
-        $("img[src*='moto01']").hide();
+        $("img[src*='moto01']").hide(3000);
+    });
+
+    $("#show").click(function(){
+        $("img[src*='moto01']").show(3000);
+    });
+
+    $("#slideUp").click(function(){
+        $("img[src*='moto02']").slideUp();
+    });
+
+    $("#slideDown").click(function(){
+        $("img[src*='moto02']").slideDown();
+    });
+
+    $("#fadeOut").click(function(){
+        $("img[src*='moto03']").fadeOut();
+    });
+
+    $("#fadeIn").click(function(){
+        $("img[src*='moto03']").fadeIn();
+    });
+
+    $("#toggle").click(function(){
+        $("img[src*='moto04']").toggle();
     });
 });
